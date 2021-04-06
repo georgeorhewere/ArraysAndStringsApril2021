6b643ca2320e694f808caba5ad7b65f0ad8744c9
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-04-07 00:10:23 +0100

    select pivot at midpoint of the array

--- ArraysAndStringsApril2021/Program.cs
+++ ArraysAndStringsApril2021/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ArraysAndStringsApril2021.Sorting;
+using System;
 
 namespace ArraysAndStringsApril2021
 {
@@ -7,6 +8,17 @@ namespace ArraysAndStringsApril2021
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            testQuickSortMidPivot();
+        }
+
+
+        static void testQuickSortMidPivot()
+        {
+            int[] numbers = new int[] {7,1,3,2,4, 5,6 };
+            QuickSortMidPivot manager = new QuickSortMidPivot(numbers);
+            manager.sort();
+
+
         }
     }
 }
--- ArraysAndStringsApril2021/Sorting/QuickSortMidPivot.cs
+++ ArraysAndStringsApril2021/Sorting/QuickSortMidPivot.cs
@@ -8,5 +8,37 @@ namespace ArraysAndStringsApril2021.Sorting
 {
     public class QuickSortMidPivot
     {
+        private int[] quickSortArray;
+
+        public QuickSortMidPivot(int [] numberList)
+        {
+            quickSortArray = numberList;
+        }
+
+        public void sort()
+        {            
+            quickSortUtility(quickSortArray, 0, quickSortArray.Length - 1);
+        }
+
+
+        private void quickSortUtility(int[] arr, int lower, int upper)
+        {
+            // condition to stop the recursion
+            if (upper <= lower)
+            {
+                return;
+            }
+
+            // select a pivot at the middle of the array
+            int mid = lower + (upper - lower) / 2; // To avoid the overflow
+            int pivot = arr[mid];
+            int start = lower;
+            int stop = upper;
+
+            Console.WriteLine($"Mid Point {mid} Pivot {pivot}");
+
+
+        }
+
     }
 }
