29e0da0e86a4407e747b70bdc347009ec72e1335
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-04-13 19:23:56 +0100

    use d a while loop for check but still met with timeout on submission

--- ArraysAndStringsApril2021/Strings/AlternateChars.cs
+++ ArraysAndStringsApril2021/Strings/AlternateChars.cs
@@ -26,26 +26,8 @@ namespace ArraysAndStringsApril2021
                 Console.WriteLine("process string");
                 StringBuilder builder = new StringBuilder(s.Length);
                 // BruteForceCharCheck(s, builder);
-                int index = 0;
-                int skip = 0;
-
-                while (index < s.Length)
-                {
-                    var DistinctChars = s.Skip(skip).TakeWhile(x =>
-                    {
-                        bool retVal;
-                        retVal = index == 0 || (index > 0 && builder.ToString().Last() != x);
-                        Console.WriteLine($"index {index} x { x } retVal {retVal} ");
-                        if (retVal)
-                            builder.Append(x);
-                        index++;
-                        skip = index;
-                        return retVal;
-                    }).ToList();
-
-                    var t =  new String(DistinctChars.ToArray());
-                }
-                //builder = new StringBuilder(new String(DistinctChars.ToArray()));
+                //TakeWhileCharCheck(s, builder);
+                WhileLoopCharCheck(s, distinctCharGroups, builder);
 
 
 
@@ -55,6 +37,48 @@ namespace ArraysAndStringsApril2021
 
             Console.WriteLine($"The minimum deletions for {s} is {minimumDeletions}");
             return minimumDeletions;
+        }
+
+        private static void WhileLoopCharCheck(string s, IEnumerable<IGrouping<char, char>> distinctCharGroups, StringBuilder builder)
+        {
+            var distinctChars = distinctCharGroups.Select(x => x.Key).ToArray();
+            builder.Append(s.First());
+            int start = 1;
+            while (start < s.Length)
+            {
+                char check = distinctChars.Where(s => s != builder.ToString().Last()).First();
+                var nextIndex = s.IndexOf(check, start);
+                if (nextIndex > -1)
+                {
+                    builder.Append(check);
+                    start = nextIndex + 1;
+                }
+                else
+                    break;
+            }
+        }
+
+        private static void TakeWhileCharCheck(string s, StringBuilder builder)
+        {
+            int index = 0;
+            int skip = 0;
+
+            while (index < s.Length)
+            {
+                var DistinctChars = s.Skip(skip).TakeWhile(x =>
+                {
+                    bool retVal;
+                    retVal = index == 0 || (index > 0 && builder.ToString().Last() != x);
+                    Console.WriteLine($"index {index} x { x } retVal {retVal} ");
+                    if (retVal)
+                        builder.Append(x);
+                    index++;
+                    skip = index;
+                    return retVal;
+                }).ToList();
+
+                var t = new String(DistinctChars.ToArray());
+            }
         }
 
         private static void BruteForceCharCheck(string s, StringBuilder builder)
