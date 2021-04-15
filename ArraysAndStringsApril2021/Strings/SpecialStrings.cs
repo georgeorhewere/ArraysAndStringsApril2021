54cb66e2c27c731ab251b75974649cfeb0cf032a
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-04-15 19:12:37 +0100

    initial special string template

---
+++ ArraysAndStringsApril2021/Strings/SpecialStrings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndStringsApril2021.Strings
+{
+
+    // return the number of substrings in a string that meet the following conditions
+    /// <summary>
+    /// a. all the characters are the same 
+    /// b. the string is separated in the middle by a single character
+    /// </summary>
+    public class SpecialStrings
+    {
+        static long substrCount(int n, string s)
+        {
+
+            return 1;
+        }
+    }
+}
