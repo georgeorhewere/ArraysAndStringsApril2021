39e8432165ff04a9b9c248520be10aabaca5b2e4
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-04-18 19:47:46 +0100

    removed unneeded code but it still requires optimization. timeout on more than half the test cases on submission

--- ArraysAndStringsApril2021/Strings/SpecialStrings.cs
+++ ArraysAndStringsApril2021/Strings/SpecialStrings.cs
@@ -21,46 +21,37 @@ namespace ArraysAndStringsApril2021
             var timer = new Stopwatch();
             List<string> subStringPerms = new List<string>();
             timer.Start();
-            for(int x =0; x < n; x++)
+            int numOfSpecialSubstrings = 0;
+            for (int x = 0; x < n; x++)
             {
                 //add current char
                 //subStringPerms.Add(s[x].ToString());
                 //get permutations
                 int count = x + 1;
-                    for(int r = 0; r < x + 1; r++)
-                    {
-                        string substring = s.Substring(r,count);                        
-                        subStringPerms.Add(substring);             
-                        count--;
-                    }
-                
-            }
-
-            int numOfSpecialSubstrings = 0;
-            timer.Stop();
-            Console.WriteLine($"number of permutations - {subStringPerms.Count()} time taken - { timer.ElapsedMilliseconds }");
-            //timer.Reset();
-            //timer.Start();
-            foreach (string d in subStringPerms)
-            {                
-                if (d.All(f => f == d.First()))
+                for (int r = 0; r < x + 1; r++)
                 {
-                    Console.WriteLine(d);
-                    numOfSpecialSubstrings++;
-                }
-                else if (d.Length > 2)
-                {
-                    int midpoint = d.Length / 2;
-                    Console.WriteLine($"midpoint of { d } is { midpoint } value- {d[midpoint]} left {d.Substring(0, midpoint)} right - {d.Substring(midpoint + 1)} ");
-                    if (d.Substring(0, midpoint).Equals(d.Substring(midpoint +1)))
-                    {
+                    string substring = s.Substring(r, count);
+                    if (substring.All(f => f == substring.First()))
+                    {            
                         numOfSpecialSubstrings++;
                     }
+                    else if (substring.Length > 2)
+                    {
+                        int midpoint = substring.Length / 2;
+                        //Console.WriteLine($"midpoint of { d } is { midpoint } value- {d[midpoint]} left {d.Substring(0, midpoint)} right - {d.Substring(midpoint + 1)} ");
+                        if (substring.Substring(0, midpoint).Equals(substring.Substring(midpoint + 1)))
+                        {
+                            numOfSpecialSubstrings++;
+                        }
+                    }                    
+                    count--;
                 }
-            }
-            //timer.Stop();
-          //  Console.WriteLine($"Elapsed time for condition check {timer.ElapsedMilliseconds}");
 
+            }
+
+
+            timer.Stop();
+            Console.WriteLine($"number of permutations - {subStringPerms.Count()} time taken - { timer.ElapsedMilliseconds }");
 
             return numOfSpecialSubstrings;
         }
