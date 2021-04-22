7e865689b124bf81cd2040ba14dc60a1d5cfd297
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-04-22 12:37:35 +0100

    Base template for array problem in number of forward switched

---
+++ ArraysAndStringsApril2021/Arrays/NewYearMix.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndStringsApril2021.Arrays
+{
+    public class NewYearMix
+    {
+
+        // Complete the minimumBribes function below.
+        //Print minimum number of forward switches in the array
+        // Print 'Too Chaotic' for any switch that is more than 2
+        static void minimumBribes(int[] q)
+        {
+            q.ToList().ForEach(Console.WriteLine);
+            Console.WriteLine("Manage Array");
+
+        }
+    }
+}
